c818658e8758573febeefd35d822bb7edd84714c
晋 <1540175452@qq.com>
2022-09-20 13:45:17 +0800

    优化余额操作器

--- src/Thinkphp/Bus/Balance/BalanceRepository.php
+++ src/Thinkphp/Bus/Balance/BalanceRepository.php
@@ -18,183 +18,184 @@ use Xin\Support\Str;
 
 class BalanceRepository implements BalanceRepositoryContract
 {
-	use WithConfig;
+    use WithConfig;
 
-	/**
-	 * @var array
-	 */
-	protected $config = [];
+    /**
+     * @var array
+     */
+    protected $config = [];
 
-	/**
-	 * Balance constructor.
-	 *
-	 * @param array $config
-	 */
-	public function __construct($config = [])
-	{
-		$this->config = $config;
-	}
+    /**
+     * Balance constructor.
+     *
+     * @param array $config
+     */
+    public function __construct(array $config = [])
+    {
+        $this->config = $config;
+    }
 
-	/**
-	 * @inheritDoc
-	 */
-	public function recharge($userId, $amount, $remark = '', $attributes = [], $logAttributes = [])
-	{
-		$query = $this->newQuery()->where('id', $userId)->inc($this->field(), $amount);
+    /**
+     * @inheritDoc
+     */
+    public function recharge($userId, $amount, $remark = '', $attributes = [], $logAttributes = [])
+    {
+        $query = $this->newQuery()->where('id', $userId)->inc($this->field(), $amount);
 
-		$fieldTotal = $this->fieldTotal();
-		if ($fieldTotal) {
-			$query = $query->inc($fieldTotal, $amount);
-		}
+        $fieldTotal = $this->fieldTotal();
+        if ($fieldTotal) {
+            $query = $query->inc($fieldTotal, $amount);
+        }
 
-		$result = $query->update($attributes);
-		if (!$result) {
-			throw new BalanceModifyException("余额变更失败！");
-		}
+        $result = $query->update($attributes);
+        if (!$result && $amount > 0) {
+            throw new BalanceModifyException("余额变更失败！");
+        }
 
-		$logData = $this->insertLog($userId, 1, $amount, $remark, $logAttributes);
+        $logData = $this->insertLog($userId, 1, $amount, $remark, $logAttributes);
 
-		$logData['current'] = $value = $this->value($userId);
+        $logData['current'] = $value = $this->value($userId);
 
-		$this->triggerEvent($logData);
+        $this->triggerEvent($logData);
 
-		return $value;
-	}
+        return $value;
+    }
 
-	/**
-	 * @inheritDoc
-	 */
-	public function consume($userId, $amount, $remark = '', $attributes = [], $logAttributes = [])
-	{
-		$value = $this->newQuery()->where('id', $userId)->value($this->field());
-		if ($value < $amount) {
-			throw new BalanceInsufficientException("余额不足！");
-		}
+    /**
+     * @inheritDoc
+     */
+    public function consume($userId, $amount, $remark = '', $attributes = [], $logAttributes = [])
+    {
+        $value = $this->newQuery()->where('id', $userId)->value($this->field());
+        if ($value < $amount) {
+            throw new BalanceInsufficientException("余额不足！");
+        }
 
-		$result = $this->newQuery()->where('id', $userId)->dec($this->field(), $amount)->update($attributes);
-		if (!$result) {
-			throw new BalanceModifyException("余额变更失败！", 40200);
-		}
+        $result = $this->newQuery()->where('id', $userId)->dec($this->field(), $amount)->update($attributes);
 
-		$logData = $this->insertLog($userId, 0, $amount, $remark, $logAttributes);
-		$logData['current'] = $value - $amount;
+        if (!$result && $amount > 0) {
+            throw new BalanceModifyException("余额变更失败！", 40200);
+        }
 
-		$this->triggerEvent($logData);
+        $logData = $this->insertLog($userId, 0, $amount, $remark, $logAttributes);
+        $logData['current'] = $value - $amount;
 
-		return true;
-	}
+        $this->triggerEvent($logData);
 
-	/**
-	 * @inheritDoc
-	 */
-	public function value($userId)
-	{
-		return (float)$this->newQuery()->where('id', $userId)->value($this->field(), 0);
-	}
+        return true;
+    }
 
-	/**
-	 * 插入记录
-	 *
-	 * @param int $userId
-	 * @param string $type
-	 * @param float $amount
-	 * @param string $remark
-	 * @param array $attributes
-	 * @return array
-	 */
-	protected function insertLog($userId, $type, $amount, $remark = '', $attributes = [])
-	{
-		$data = array_merge($attributes, [
-			'log_no' => Str::makeOrderSn(),
-			'user_id' => $userId,
-			'type' => $type,
-			$this->logField() => $amount,
-			'remark' => $remark,
-			'create_time' => time(),
-		]);
+    /**
+     * @inheritDoc
+     */
+    public function value($userId)
+    {
+        return (float)$this->newQuery()->where('id', $userId)->value($this->field(), 0);
+    }
 
-		$this->logQuery()->insert($data);
+    /**
+     * 插入记录
+     *
+     * @param int $userId
+     * @param string $type
+     * @param float $amount
+     * @param string $remark
+     * @param array $attributes
+     * @return array
+     */
+    protected function insertLog($userId, $type, $amount, $remark = '', $attributes = [])
+    {
+        $data = array_merge($attributes, [
+            'log_no' => Str::makeOrderSn(),
+            'user_id' => $userId,
+            'type' => $type,
+            $this->logField() => $amount,
+            'remark' => $remark,
+            'create_time' => time(),
+        ]);
 
-		return $data;
-	}
+        $this->logQuery()->insert($data);
 
-	/**
-	 * 获取余额字段
-	 *
-	 * @return string
-	 */
-	protected function field()
-	{
-		return $this->getConfig('field', 'balance');
-	}
+        return $data;
+    }
 
-	/**
-	 * 获取累计余额字段（累计余额字段只增不减）
-	 *
-	 * @return string
-	 */
-	protected function fieldTotal()
-	{
-		return $this->getConfig('field_total', null);
-	}
+    /**
+     * 获取余额字段
+     *
+     * @return string
+     */
+    protected function field()
+    {
+        return $this->getConfig('field', 'balance');
+    }
 
-	/**
-	 * 获取日志余额字段
-	 *
-	 * @return string
-	 */
-	protected function logField()
-	{
-		return $this->getConfig('log.field', $this->field());
-	}
+    /**
+     * 获取累计余额字段（累计余额字段只增不减）
+     *
+     * @return string
+     */
+    protected function fieldTotal()
+    {
+        return $this->getConfig('field_total', null);
+    }
 
-	/**
-	 * 触发事件
-	 *
-	 * @param array $logData
-	 */
-	protected function triggerEvent($logData)
-	{
-		$eventClass = $this->getConfig('event');
-		if (!$eventClass) {
-			return;
-		}
+    /**
+     * 获取日志余额字段
+     *
+     * @return string
+     */
+    protected function logField()
+    {
+        return $this->getConfig('log.field', $this->field());
+    }
 
-		$logData['field'] = $this->field();
+    /**
+     * 触发事件
+     *
+     * @param array $logData
+     */
+    protected function triggerEvent($logData)
+    {
+        $eventClass = $this->getConfig('event');
+        if (!$eventClass) {
+            return;
+        }
 
-		$app = App::getInstance();
-		$event = $app->invokeClass($eventClass, [$logData]);
-		$app->event->trigger($event);
-	}
+        $logData['field'] = $this->field();
 
-	/**
-	 * 获取模型查询实例
-	 *
-	 * @return Query|Db
-	 */
-	protected function newQuery()
-	{
-		$modelClass = $this->getConfig('model');
-		if (!$modelClass) {
-			throw new \RuntimeException("balance model not defined.");
-		}
+        $app = App::getInstance();
+        $event = $app->invokeClass($eventClass, [$logData]);
+        $app->event->trigger($event);
+    }
 
-		return (new $modelClass)->db();
-	}
+    /**
+     * 获取模型查询实例
+     *
+     * @return Query|Db
+     */
+    protected function newQuery()
+    {
+        $modelClass = $this->getConfig('model');
+        if (!$modelClass) {
+            throw new \RuntimeException("balance model not defined.");
+        }
 
-	/**
-	 * @return \think\Db|\think\db\Query|\think\facade\Db
-	 */
-	protected function logQuery()
-	{
-		$config = $this->getConfig('log');
-		if ($config['type'] === 'model') {
-			$modelClass = $config['model'];
+        return (new $modelClass)->db();
+    }
 
-			return (new $modelClass)->db();
-		}
+    /**
+     * @return \think\Db|\think\db\Query|\think\facade\Db
+     */
+    protected function logQuery()
+    {
+        $config = $this->getConfig('log');
+        if ($config['type'] === 'model') {
+            $modelClass = $config['model'];
 
-		return Db::name($config['table']);
-	}
+            return (new $modelClass)->db();
+        }
+
+        return Db::name($config['table']);
+    }
 
 }
